1214bae50ae2470e290130123aa1b1fc00895805
haileyport <sugar9935@gmail.com>
2022-10-06 18:50:44 +0900

    🐛 BUG:  delete Duplicate import

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
-import Gnb from "./components/Gnb";
-import Header from "./components/Header";
-import './style/common.css'
-import LoadingPage from "./components/Loading/LoadingPage";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Gnb from './components/Gnb';
+import Header from './components/Header';
+import './style/common.css';
+import LoadingPage from './components/Loading/LoadingPage';
 
 function App() {
-
   return (
-      <BrowserRouter>
-        <LoadingPage />
-        <Header />
-        <Gnb />
-        <Routes>
-          <Route path="/" element={<Home />}> </Route>
-        </Routes>
-
-      </BrowserRouter>
+    <BrowserRouter>
+      <LoadingPage />
+      <Header />
+      <Gnb />
+      <Routes>
+        <Route path='/' element={<Home />}>
+          {' '}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
