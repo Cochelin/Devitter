0126d807d5bd7c9af8739058db7c3f6e997b8962
haileyport <sugar9935@gmail.com>
2022-10-13 19:05:30 +0900

    🔨 Script:  test http url

--- src/components/Twit.js
+++ src/components/Twit.js
@@ -103,41 +103,52 @@ export const Twit = ({ profile, name, id, contents, retweet, likes, link }) => {
 
   //미완성 http 가 있으면 slice 후 a 태그로 감싸고 싶음
   // link 생성은 완료했는데 앞  string 하고 합치면 [object Object] 로 변경되는 현상 발생
-  // console.log(contents)
-  const contentLink = () => {
-    if (contents.indexOf('http') !== -1) {
-      let topContents = contents.slice(0, contents.indexOf('http'));
-      let linkContent = contents.slice(contents.indexOf('http'));
-      console.log(topContents);
+  // const contentLink = () => {
+  //   if (contents.indexOf('http') !== -1) {
+  //     let topContents = contents.slice(0, contents.indexOf('http'));
+  //     let linkContent = contents.slice(contents.indexOf('http'));
+  //     if (linkContent.indexOf(' ') !== -1) {
+  //       linkContent = linkContent.slice(0, linkContent.indexOf(' '));
+  //       let lastConents = linkContent.slice(linkContent.indexOf(' ') - 1);
+  //       let linkContentLink = { __html: linkContent };
+  //       return (contents = (
+  //         <>
+  //           {topContents}
+  //           <a
+  //             href={linkContent}
+  //             target='_blank'
+  //             rel='noopener noreferrer'
+  //             dangerouslySetInnerHTML={linkContentLink}
+  //           />
+  //           {lastConents}
+  //         </>
+  //       ));
+  //     } else {
+  //       let linkContentLink = { __html: linkContent };
+  //       return (contents = (
+  //         <>
+  //           {topContents}
+  //           <a
+  //             href={linkContent}
+  //             target='_blank'
+  //             rel='noopener noreferrer'
+  //             dangerouslySetInnerHTML={linkContentLink}
+  //           />
+  //         </>
+  //       ));
+  //     }
+  //   }
+  // };
 
-      if (linkContent.indexOf(' ') !== -1) {
-        linkContent = linkContent.slice(0, linkContent.indexOf(' '));
-        let lastConents = linkContent.slice(linkContent.indexOf(' ') - 1);
-        let linkContentLink = { __html: linkContent };
-        return (contents = (
-          <a
-            href={linkContent}
-            target='_blank'
-            rel='noopener noreferrer'
-            dangerouslySetInnerHTML={linkContentLink}
-          />
-        ));
-      } else {
-        let linkContentLink = { __html: linkContent };
-        return (contents =
-          topContents +
-          (
-            <a
-              href={linkContent}
-              target='_blank'
-              rel='noopener noreferrer'
-              dangerouslySetInnerHTML={linkContentLink}
-            />
-          ));
-      }
-    }
-  };
-  // contentLink()
+  // const urlRegex = /(https?:\/\/[^\s]+)/g;
+  // contents = contents.replace(urlRegex, (url) => {
+  //   return (
+  //     <Link href={url} target='_blank' rel='noopener noreferrer'>
+  //       {url}
+  //     </Link>
+  //   );
+  // });
+  // console.log('1', contents);
 
   return (
     <Box>
@@ -159,8 +170,7 @@ export const Twit = ({ profile, name, id, contents, retweet, likes, link }) => {
       <Bottom>
         <IconContainer>
           <LinkIconA href={link} target='_blank' rel='noopener noreferrer'>
-            {' '}
-            <LinkIcon />{' '}
+            <LinkIcon />
           </LinkIconA>
         </IconContainer>
         <IconContainer>
