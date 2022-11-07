46e7f3c4ecde7e1667dd04b484d95da618119bf4
Ryu heekyung <beals1129@gmail.com>
2022-11-07 12:54:28 +0900

    😢Test : useAxios

--- src/App.js
+++ src/App.js
@@ -8,13 +8,16 @@ import LoadingPage from './components/Loading/LoadingPage';
 import Bookmark from './pages/Bookmark';
 import BookMarkContainer from './components/BookMarkContainer';
 import TwittCompo from './pages/TwittCompo';
-
+import { useCallback, useState } from 'react';
 function App() {
+  //패이지 active 를 위한 재랜더링
+  const [, updateState] = useState()
+  const forceUpdate = useCallback(() => updateState({}), [])
   return (
     <BrowserRouter>
-      {/* <LoadingPage /> */}
-      <Header />
-      <Gnb />
+      <LoadingPage />
+      <Header forceUpdate={forceUpdate} />
+      <Gnb forceUpdate={forceUpdate} />
       <Routes>
         <Route path='/' element={<Home />} >
           <Route path='/' element={<TwittCompo />} />
@@ -24,6 +27,7 @@ function App() {
         <Route path='/Bookmark' element={<Bookmark />} >
           <Route path=":mainId/:subId" element={<BookMarkContainer />} />
         </Route>
+
 
         {/* <Route path='/' element={<Navigate to='/' />} /> */}
       </Routes>
